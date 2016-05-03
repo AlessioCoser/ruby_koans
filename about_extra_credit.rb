8b02146a2e24ec541f7b51ecf8bff97449d0f4f5
ale.co1989@gmail.com <Pot_Anglon89>
2016-05-03 17:53:16 +0200

    begin GreedGame

--- about_extra_credit.rb
+++ about_extra_credit.rb
@@ -1,3 +1,5 @@
+require File.expand_path(File.dirname(__FILE__) + '/neo')
+
 # EXTRA CREDIT:
 #
 # Create a program that will play the Greed Game.
@@ -7,7 +9,13 @@
 # Write a player class and a Game class to complete the project.  This
 # is a free form assignment, so approach it however you desire.
 
+class GreedGame
+
+end
 
 class GreedGameAsserts < Neo::Koan
-
+  def initialize_greed_game
+    game = GreedGame.new
+    assert_equal GreedGame, game.class
+  end
 end
